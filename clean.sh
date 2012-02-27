b5be7944856fd9965d05a9611c72a5d6230ad7e6
kiaruska <vittlc@gmail.com>
2012-02-27 17:05:33 +0100

    ordered files

--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#script dei i3terroni modificato
-
-#LIST=$(ls | grep block)
-LIST="./data/allusers/allusers.filtered"
-# sed 's/#//g' #per togliere anche l' "#" dal testo
-for i in $LIST; do cat $i | sed 's/@[^ ]*//g' | sed 's/#//g' | sed -r 's@https?://[^ ]*@@g' | sed "/^[[:digit:]]*\s[[:digit:]]*\s*$/d" > "./data/allusers/allusers.filtered.cleaned"; done
