ea5b2083afcda90e8eabd3e4be7293a113583333
Emilio <emilio.deltessa@gmail.com>
2012-03-15 19:57:29 +0100

    added frequency plot script

---
+++ hashtags/plot/build.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+pdflatex -shell-escape hashtag.tex
+rm hashtag-figure0.d* *.log *.aux* hashtag.pdf
---
+++ hashtags/plot/plotFrequency.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2.6
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+import codecs
+import toUnicode
+
+class tagAnnotation(object):
+
+	def __init__(self, filetext):
+		self.fileT = filetext
+		self.hashtags = dict()
+
+	def parse(self):
+		self.dataT = open(self.fileT)	
+		self.parse_data()
+		self.dataT.close()
+
+	def print_data( self, dictionary, fileSuffix, keyPrefix, valuePrefix ):
+		name, ext = os.path.splitext(self.filename)
+		out = open( name + fileSuffix, 'w')
+		
+		#stampo un file a due colonne (per gnuplot)
+		#indice[TAB]score[\n]
+		
+		#out.write( "hashtag\tscore\n" )
+		sortedHashtags = sorted( dictionary, key=lambda x: -dictionary[x] )
+		for h in sortedHashtags:
+			out.write( "{0}\t{1}\n".format( h, dictionary[h] ) )
+		
+		out.close()
+
+	def parse_data(self):
+		counter = 0
+		
+		print( "Extracting informations from tweet file..." )
+		userID_old = ""
+		#hashtagAnn = dict()
+		for text_tweet in self.dataT:
+			counter += 1
+			if counter % 10000 == 0:
+				print( "Read {0} lines (current user: {1}).".format( counter, userID_old ) )
+			
+			#XXX non ha nessun senso convertire a unicode (filtrando eventuali codici html) se l'annotazione
+			#e' gia' stata lanciata su questo file "sporco".. Al limite l'annotazione dovrebbe essere lanciata
+			#dopo aver pulito il testo
+			#text_tweet = toUnicode.toUnicode(text_tweet) 		#chiamo la funzione per convertire
+			time, userID, text = text_tweet.split(None, 2) 		#None spezza sui caratteri bianchi
+			tags = re.findall('#\w+', text.lower() )		#prendo gli hashtags
+			
+			if len(tags) == 0:
+				continue
+			
+			for t in tags :						#aggiungo le annotazioni nel dizionario
+				if t in self.hashtags:
+					self.hashtags[t] = self.hashtags + 1
+				else: 	self.hashtags[t] = 1
+		
+		self.print_data( self.hashtags, ".plot", '', '' )	
+		print( "Done." )	
+
+if __name__ == '__main__':
+	filetext = sys.argv[1]
+	parser = tagAnnotation(filetext)
+	parser.parse()
