922225004180f9d84ba782c2b15ed2f5b3035572
kiaruska <vittlc@gmail.com>
2012-03-15 19:59:51 +0100

    ...

--- hashtags/plot/plotFrequency.py
+++ hashtags/plot/plotFrequency.py
@@ -5,7 +5,7 @@ import os
 import re
 import sys
 import codecs
-import toUnicode
+#import toUnicode
 
 class tagAnnotation(object):
 
@@ -19,7 +19,7 @@ class tagAnnotation(object):
 		self.dataT.close()
 
 	def print_data( self, dictionary, fileSuffix, keyPrefix, valuePrefix ):
-		name, ext = os.path.splitext(self.filename)
+		name, ext = os.path.splitext(self.fileT)
 		out = open( name + fileSuffix, 'w')
 		
 		#stampo un file a due colonne (per gnuplot)
@@ -36,12 +36,11 @@ class tagAnnotation(object):
 		counter = 0
 		
 		print( "Extracting informations from tweet file..." )
-		userID_old = ""
 		#hashtagAnn = dict()
 		for text_tweet in self.dataT:
 			counter += 1
 			if counter % 10000 == 0:
-				print( "Read {0} lines (current user: {1}).".format( counter, userID_old ) )
+				print( "Read {0} lines.".format( counter ) )
 			
 			#XXX non ha nessun senso convertire a unicode (filtrando eventuali codici html) se l'annotazione
 			#e' gia' stata lanciata su questo file "sporco".. Al limite l'annotazione dovrebbe essere lanciata
@@ -55,7 +54,7 @@ class tagAnnotation(object):
 			
 			for t in tags :						#aggiungo le annotazioni nel dizionario
 				if t in self.hashtags:
-					self.hashtags[t] = self.hashtags + 1
+					self.hashtags[t] = self.hashtags[t] + 1
 				else: 	self.hashtags[t] = 1
 		
 		self.print_data( self.hashtags, ".plot", '', '' )	
